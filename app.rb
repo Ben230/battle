ce5f87c1e1af4245362fe4a4c4ee84c675eaa0a4
Ben Stuart <benarthurstuart@hotmail.com>
2019-11-02 11:03:45 +0000

    Finished! - global variable gone

--- app.rb
+++ app.rb
@@ -5,33 +5,34 @@ require './lib/game'
 class Battle < Sinatra::Base
   enable :sessions
 
+  before do
+    @game = Game.instance
+  end
+
   get '/' do
     erb :index
   end
 
   post '/names' do
-    $game = Game.new(Player.new(params[:player_1_name]), Player.new(params[:player_2_name]))
+    @game = Game.create(Player.new(params[:player_1_name]), Player.new(params[:player_2_name]))
     redirect '/play'
   end
 
   get '/play' do
-    @game = $game
     erb :play
   end
 
   get '/attack' do
-    @game = $game
     @game.attack(@game.opponent)
     erb :attack
   end
 
   post '/switch-turns' do
-    $game.switch_turns
+    @game.switch_turns
     redirect '/play'
   end
 
   get '/game_over' do
-    @game = $game
     erb :game_over
   end
 
